ddc7f063fd4ba949caf95a55aa542c8584cece39
harshit-ttn <99420919+harshit-ttn@users.noreply.github.com>
2022-03-26 17:21:10 +0530

    Counting

--- index.js
+++ index.js
@@ -31,22 +31,11 @@ async function createCourse(){
 
 async function getCourses(){
     const courses = await Course
-    // .find({author:'Mosh',isPublished:true})
-
-    // Starts with Mosh
-    .find({ author: /^Mosh/ })
-
-    // Ends with Hamedani
-    // i for case insensitive
-    .find({ author: /Hamedani$/i })
-
-    // Contains Mosh
-    .find({ author: /.*Mosh.*/i })
-
-    
+    .find({author:'Mosh',isPublished:true})
     .limit(10)
     .sort({name:1})
-    .select({name:1,tags:1});
+    .select({name:1,tags:1})
+    .count()
     console.log(courses);
 }
 
