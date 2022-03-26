aa3faa1f6b2c98cfa1a71f08c910a9624049836c
harshit-ttn <99420919+harshit-ttn@users.noreply.github.com>
2022-03-26 17:40:41 +0530

    Pagination

--- index.js
+++ index.js
@@ -30,9 +30,14 @@ async function createCourse(){
 
 
 async function getCourses(){
+    const pageNumber = 2;
+    const pageSize = 10;
+    
+
     const courses = await Course
     .find({author:'Mosh',isPublished:true})
-    .limit(10)
+    .skip((pageNumber - 1)* pageSize)
+    .limit(pageSize)
     .sort({name:1})
     .select({name:1,tags:1})
     .count()
